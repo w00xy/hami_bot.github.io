6e2a0da9adc539bc9bb5c4d6b486659a0fcee4a4
l0yzi <sinnloseexistenz@gmail.co>
2024-04-13 23:33:03 +0300

    1

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-var setElementHeight = function () {
-    var height = $(window).height();
-    $('.autoheight').css('min-height', (height));
-};
+// screens
+let mainScreen = document.getElementById('main_screen')
+let storageScreen = document.getElementById('storage_screen')
 
-$(window).on("resize", function () {
-    setElementHeight();
-}).resize();
+// Кнопки
+let btnStorage = document.getElementById('storage_btn')
+
+
+
+function getStorage () {
+    mainScreen.classList.remove('visible_main_screen')
+    mainScreen.classList.add('hide')
+    storageScreen.classList.remove('hide')
+    storageScreen.classList.add('visible_storage_screen')
+}
